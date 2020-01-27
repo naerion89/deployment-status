21c89facd78e6eb31c3268ddf38a1d18e8799e77
Gaetan BOGAERT <30294729+naerion89@users.noreply.github.com>
2020-01-27 13:20:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,37 +2,64 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 async function run() {
-  try {
-    const context = github.context;
-    const defaultUrl = `https://github.com/${context.repo.owner}/${context.repo.repo}/commit/${context.sha}/checks`;
+    try {
+        const context = github.context;
+        const defaultUrl = `https://github.com/${context.repo.owner}/${context.repo.repo}/commit/${context.sha}/checks`;
 
-    const token = core.getInput("token", {required: true});
-    const state = core.getInput("state", {required: true});
-    const url = core.getInput("log-url", {required: false}) || defaultUrl;
-    const description = core.getInput("description", {required: false});
-    const env = core.getInput("environment", {required: false});
-    const envUrl = core.getInput("environment-url", {required: false});
+        const token = core.getInput("token", {
+            required: true
+        });
+        const state = core.getInput("state", {
+            required: true
+        });
+        const url = core.getInput("log-url", {
+            required: false
+        }) || defaultUrl;
+        const description = core.getInput("description", {
+            required: false
+        });
+        const env = core.getInput("environment", {
+            required: false
+        });
+        const envUrl = core.getInput("environment-url", {
+            required: false
+        });
 
-    const client = new github.GitHub(token);
-    const params = {
-      ...context.repo,
-      deployment_id: context.payload.deployment.id,
-      state,
-      log_url: url,
-      target_url: url,
-      description,
-    };
-    if (env) {
-      params.environment = env;
+        const client = new github.GitHub(token);
+        if (context.payload.deployment === undefined) {
+            const params = {
+                ...context.repo,
+
+                state,
+            };
+            if (env) {
+                params.environment = env;
+            }
+            if (envUrl) {
+                params.environment_url = envUrl;
+            }
+            await client.repos.createDeployment(params);
+        } else {
+            const params = {
+                ...context.repo,
+                deployment_id: context.payload.deployment.id,
+                state,
+                log_url: url,
+                target_url: url,
+                description,
+            };
+            if (env) {
+                params.environment = env;
+            }
+            if (envUrl) {
+                params.environment_url = envUrl;
+            }
+            await client.repos.createDeploymentStatus(params);
+        }
+    } catch (error) {
+        core.error(error);
+        core.setFailed(error.message);
     }
-    if (envUrl) {
-      params.environment_url = envUrl;
-    }
-    await client.repos.createDeploymentStatus(params);
-  } catch (error) {
-    core.error(error);
-    core.setFailed(error.message);
-  }
 }
 
 run();
